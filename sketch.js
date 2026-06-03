9f916791a26fe58e5544d596b27d7066189bf72b
agrinho_programacao2025 <mariane.ferreira1222@gmail.com>
2026-06-03 09:10:30 -0300

    teste 3

--- sketch.js
+++ sketch.js
@@ -36,72 +36,26 @@ function draw() {
       folha.x = random(width);
     }
   }
-const perguntas = [
-{
-    pergunta: "Qual prática ajuda a conservar o solo?",
-    opcoes: ["Plantio Direto", "Queimadas", "Desmatamento"],
-    correta: 0
-},
-{
-    pergunta: "O que a rotação de culturas faz?",
-    opcoes: ["Empobrece o solo", "Melhora a fertilidade", "Aumenta a erosão"],
-    correta: 1
-},
-{
-    pergunta: "Qual sistema combina agricultura, pecuária e árvores?",
-    opcoes: ["ILPF", "Monocultura", "Queimada Controlada"],
-    correta: 0
-},
-{
-    pergunta: "O que reduz o impacto ambiental no campo?",
-    opcoes: ["Uso consciente da água", "Desmatamento", "Poluição dos rios"],
-    correta: 0
-},
-{
-    pergunta: "Qual é o objetivo da agricultura sustentável?",
-    opcoes: ["Produzir sem pensar no futuro", "Produzir preservando recursos", "Produzir menos alimentos"],
-    correta: 1
-}
-];
-
-let atual = 0;
-let pontos = 0;
-
-function carregarPergunta(){
-
-    document.getElementById("pergunta").innerHTML =
-    perguntas[atual].pergunta;
-
-    document.getElementById("op0").innerHTML =
-    perguntas[atual].opcoes[0];
-
-    document.getElementById("op1").innerHTML =
-    perguntas[atual].opcoes[1];
-
-    document.getElementById("op2").innerHTML =
-    perguntas[atual].opcoes[2];
-}
-
-function responder(escolha){
-
-    if(escolha === perguntas[atual].correta){
-        pontos++;
-    }
-
-    atual++;
-
-    if(atual < perguntas.length){
-        carregarPergunta();
-    }else{
-        document.querySelector(".quiz").innerHTML =
-        `<h1>🌾 Quiz Finalizado!</h1>
-        <h2>Você acertou ${pontos} de ${perguntas.length} perguntas!</h2>`;
-    }
-}
-
-carregarPergunta();
-
-
-function windowResized() {
+  {function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+{let pontos = 0;
+
+function responder(resposta){
+
+    let resultado = document.getElementById("resultado");
+
+    if(resposta == "certo"){
+        pontos++;
+        resultado.innerHTML = "✅ Resposta correta!";
+    }else{
+        resultado.innerHTML = "❌ Resposta incorreta!";
+    }
+}
+
+function mostrarResultado(){
+    document.getElementById("pontuacao").innerHTML =
+    "🌱 Você acertou " + pontos + " perguntas!";
+}
+
+
